72eb577edd459f729292e6daada28a9eb51fc06a
Datquangbui1011 <dbui5@huskers.unl.edu>
2025-12-06 01:46:01 -0600

    Change Release to Dashboard Admin

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
         <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to='/'>Home</Link>
         <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to='/movies'>Movies</Link>
         <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to='/theaters'>Theaters</Link>
-        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to='/releases'>Releases</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to='/admin'>Dashboard</Link>
         {favoriteMovies.length > 0 && <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to='/favorite'>Favorites</Link>}
       </div>
 
